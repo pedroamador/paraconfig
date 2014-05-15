baf963b2be337af09246759bbf05e184a8a476c8
Pedro Amador <pedroamador.rodriguez@gmail.com>
2014-05-15 13:44:02 +0200

    First functional version

--- README.md
+++ README.md
@@ -2,3 +2,14 @@ paraconfig
 ==========
 
 My little script to automate setup new machine
+
+# Use
+
+As root, execute
+	# ./paraconfig.sh [destination_host]
+
+example: 
+
+	# ./paraconfig mail
+
+You should have a key pair and the destination root user password
---
+++ paraconfig.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+## PARACONFIG ##
+basedir=$(dirname $0)
+
+echo "Paraconfig (c) Pedro Amador 2014"
+echo
+
+# Check parameters
+if [ "$#" -ne 1 ]
+then
+  echo "Usage: $0 [destination_host]" >&2
+  echo "Example: $0 mail" >&2
+  exit 1
+fi
+
+# Copy SSH key
+ssh-copy-id $1
+if [ $? -ne 0 ]
+then
+	result=$?
+	echo "Error copying ssh key"
+	exit $result
+fi
+
+# Copy apt cacher config
+rsync -a /etc/apt/apt.conf $1:/etc/apt/apt.conf &> /dev/null
+# Copy client config script
+scp $basedir/_pcclient $1:/tmp/pc_client.sh
+# Execute client config script
+ssh $1 "bash /tmp/pc_client.sh"
+exit $?
